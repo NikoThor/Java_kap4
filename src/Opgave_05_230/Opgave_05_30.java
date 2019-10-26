f555bed098e076d96753682c2d0ccfaa74d84800
Nikolaj Thor Christensen <54975617+NikoThor@users.noreply.github.com>
2019-10-26 21:17:52 +0200

    Opgave 5.30 lavet

---
+++ src/Opgave_05_230/Opgave_05_30.java
@@ -0,0 +1,25 @@
+package Opgave_05_230;
+
+import java.util.Scanner;
+
+public class Opgave_05_30 {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter an amount ");
+        double amount = input.nextDouble();
+        System.out.println("Enter the annual interest rate: ");
+        double annualInterestRate = input.nextDouble();
+        System.out.println("Enter the number of months: ");
+        int months = input.nextInt();
+
+        double monthlyInterestRate = annualInterestRate / 1200;
+        double compoundValue = 0;
+
+        for (int m = 1; m <=months; m++) {
+            compoundValue = (amount + compoundValue) *(1 + monthlyInterestRate);
+        }
+        System.out.printf(
+                "Amount in savings account after " + months + "months:  $%.2f\n", compoundValue);
+    }
+}
